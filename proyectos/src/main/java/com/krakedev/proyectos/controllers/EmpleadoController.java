99f466ec0209f9780b070665812e1f3fb5e45152
luisAristeguieta <luisjimenezgx@gmail.com>
2026-07-01 21:40:28 -0400

    feat(backend): implement cors support for react client

--- proyectos/src/main/java/com/krakedev/proyectos/controllers/EmpleadoController.java
+++ proyectos/src/main/java/com/krakedev/proyectos/controllers/EmpleadoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.krakedev.proyectos.services.EmpleadoService;
 
 @RestController
 @RequestMapping("/empleados")
+@CrossOrigin(origins = "http://localhost:5173")
 public class EmpleadoController {
 
     private final EmpleadoService servicio;
--- proyectos/src/main/java/com/krakedev/proyectos/controllers/TareaController.java
+++ proyectos/src/main/java/com/krakedev/proyectos/controllers/TareaController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.krakedev.proyectos.services.TareaService;
 
 @RestController
 @RequestMapping("/tareas")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TareaController {
 
     private final TareaService servicio;
